633fc0a0f34ff7c6e492fbd83621d97ed7e948ec
Akhilesh Bisht <150502170+akhilesh-bisht@users.noreply.github.com>
2025-03-02 23:17:37 +0530

    CHANGE CONFIG

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/portfolio/",
-	plugins: [react()],
-	resolve: {
-		alias: {
-			src: "/src",
-			components: "/src/components",
-			hooks: "/src/hooks",
-		},
-	},
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      src: "/src",
+      components: "/src/components",
+      hooks: "/src/hooks",
+    },
+  },
+});
